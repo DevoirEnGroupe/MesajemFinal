ef299e1de2a617271aba4d051ce2ee43cf14cf14
Winshelo <winshelo@hotmail.com>
2022-09-25 22:41:34 -0400

    AllActivity in Tracks

--- app/src/main/java/ht/mesajem/mesajem/Fragments/AllActivityFragment.java
+++ app/src/main/java/ht/mesajem/mesajem/Fragments/AllActivityFragment.java
@@ -2,12 +2,27 @@ package ht.mesajem.mesajem.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ht.mesajem.mesajem.Adapters.ReceiverAdapter;
+import ht.mesajem.mesajem.Models.Post;
 import ht.mesajem.mesajem.R;
 
 /**
@@ -16,6 +31,12 @@ import ht.mesajem.mesajem.R;
  * create an instance of this fragment.
  */
 public class AllActivityFragment extends Fragment {
+
+    RecyclerView rvPosts;
+    ReceiverAdapter adapter;
+    List<Post> posts;
+    Boolean mFirstLoad;
+    TextView tvreceive;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -64,4 +85,48 @@ public class AllActivityFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_all_activity, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+        tvreceive = view.findViewById(R.id.tvreceive);
+
+        posts = new ArrayList<>();
+        adapter = new ReceiverAdapter(posts,getContext());
+        rvPosts.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        rvPosts.setLayoutManager(layoutManager);
+        layoutManager.setReverseLayout(true);
+        mFirstLoad=true;
+        queryposts();
+    }
+
+    protected void queryposts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+
+                if(e == null){
+                    adapter.clear();
+                    adapter.addAll(posts);
+
+                }
+                if(mFirstLoad){
+                    rvPosts.scrollToPosition(0);
+                    mFirstLoad= false;
+                }
+                else {
+                    Log.e("post", "Error Loading post" + e);
+                }
+
+            }
+        });
+
+    }
+
+
 }
